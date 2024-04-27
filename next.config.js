a99e5874c6ef84a7bd8d4ec675b4f3526f5fad9a
Aramis <sennyeyaramis@gmail.com>
2024-04-26 20:39:51 -0400

    update to a single export
    Signed-off-by: Aramis <sennyeyaramis@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-const nextConfig = {
+module.exports = {
   // uncomment the following snippet if using styled components
   compiler: {
     styledComponents: true,
@@ -43,24 +39,4 @@ const nextConfig = {
 
     return config
   },
-}
-
-const KEYS_TO_OMIT = ['webpackDevMiddleware', 'configOrigin', 'target', 'analyticsId', 'webpack5', 'amp', 'assetPrefix']
-
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [[withBundleAnalyzer, {}]]
-
-  const wConfig = plugins.reduce((acc, [plugin, config]) => plugin({ ...acc, ...config }), {
-    ...defaultConfig,
-    ...nextConfig,
-  })
-
-  const finalConfig = {}
-  Object.keys(wConfig).forEach((key) => {
-    if (!KEYS_TO_OMIT.includes(key)) {
-      finalConfig[key] = wConfig[key]
-    }
-  })
-
-  return finalConfig
 }
